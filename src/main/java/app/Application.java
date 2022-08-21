6140d7073cd423ee0e7043796defb3164b62d968
nikolay <noisepwnz@mail.ru>
2022-08-21 18:18:14 +0500

    homework

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,12 +2,15 @@ package app;
 
 import app.config.AppConfig;
 import app.model.AnimalsCage;
+import app.model.Dog;
+import app.model.Timer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) {
+
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         for (int i = 0; i < 5; i++) {
@@ -15,6 +18,8 @@ public class Application {
                     applicationContext.getBean(AnimalsCage.class);
             bean.whatAnimalSay();
         }
+
+
     }
 
 }
